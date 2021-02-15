cad4e82e94dfd032154e38684e64d04d11a94cfa
Threon <cat4real4k@gmail.com>
2021-02-15 22:28:38 +0200

    2DShaders

---
+++ 2DGlow/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,56 @@
+﻿ using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    Animator anim;
+
+    public CharacterController2D controller;
+
+    public float runSpeed = 40f;
+
+    float horizontalMove = 0f;
+
+    bool jump = false;
+
+    float animSpeed;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal")*runSpeed;
+
+        if (horizontalMove < 0)
+        {
+            animSpeed = -horizontalMove;
+        }
+        else
+        {
+            animSpeed = horizontalMove;
+        }
+
+        anim.SetFloat("speed", animSpeed);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            anim.SetBool("Punch", true);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        controller.Move(horizontalMove*Time.fixedDeltaTime, false, jump);
+        jump = false;
+
+        anim.SetBool("Punch", false);
+    }
+}
---
+++ 2DShaderGraph/Assets/Disolve.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Disolve : MonoBehaviour
+{
+    Material material;
+
+    bool isDisolving = false;
+    float fade = 1f;
+    void Start()
+    {
+        material = GetComponent<SpriteRenderer>().material;
+    }
+
+   
+    void Update()
+    {
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            isDisolving = true;
+        }
+
+        if (isDisolving)
+        {
+            fade -= Time.deltaTime;
+            if (fade <= 0f)
+            {
+                fade = 0f;
+                isDisolving = false;
+            }
+
+            material.SetFloat("_Fade", fade);
+
+        }
+        
+    }
+}
