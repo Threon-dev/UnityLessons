bb1f9a8faf1d7038e3b9669c47792d448b3021db
Threon <cat4real4k@gmail.com>
2021-02-16 16:14:37 +0200

    TopDownMovement

---
+++ TopDownMovement/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+
+    public Rigidbody2D rb;
+    public Animator animator;
+
+    Vector2 movement;
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxis("Vertical");
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+        if (movement.x != 0)
+        {
+            transform.localScale = new Vector3(movement.x,1f,1f);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement*moveSpeed*Time.fixedDeltaTime );
+    }
+}
