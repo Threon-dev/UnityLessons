5987c6f90ac5714f3fc77a7fe6354e751aa54871
Borys Chernyshev <threondev@gmail.com>
2021-04-19 18:04:18 +0300

    NextLesson

---
+++ SortingSprites/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    private Func<Vector3> GetCameraFollowPositionFunc;
+
+    public void Setup(Func<Vector3> GetCameraFollowPositionFunc)
+    {
+        this.GetCameraFollowPositionFunc = GetCameraFollowPositionFunc;
+    }
+
+    void Update()
+    {
+        Vector3 cameraFollowPosition = GetCameraFollowPositionFunc();
+
+        cameraFollowPosition.z = transform.position.z;
+        transform.position = cameraFollowPosition;
+    }
+}
---
+++ SortingSprites/Assets/Scripts/GameHandler.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using CodeMonkey;
+using CodeMonkey.Utils;
+
+
+public class GameHandler : MonoBehaviour {
+
+    public CameraFollow cameraFollow;
+
+    public Transform playerTransform;
+
+
+    private void Start() {
+
+        cameraFollow.Setup(() => playerTransform.position);
+    }
+
+}
+
+
