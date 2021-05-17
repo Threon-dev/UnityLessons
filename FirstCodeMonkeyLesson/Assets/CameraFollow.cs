8d2602ec62a47eeecb8a2e0a6e848db5f16fa8fa
Borys Chernyshev <threondev@gmail.com>
2021-05-17 18:30:51 +0300

    CodeMonkey
    Work with Camera and Mesh

--- FirstCodeMonkeyLesson/Assets/CameraFollow.cs
+++ FirstCodeMonkeyLesson/Assets/CameraFollow.cs
@@ -5,16 +5,91 @@ using System;
 
 public class CameraFollow : MonoBehaviour
 {
+    private Camera myCamera;
     private Func<Vector3> GetFollowPositionFunc;
+    private Func<float> GetCameraZoomFunc;
 
-    public void Setup(Func<Vector3> GetFollowPositionFunc)
+    public void Setup(Func<Vector3> GetFollowPositionFunc, Func<float> GetCameraZoomFunc)
+    {
+        this.GetFollowPositionFunc = GetFollowPositionFunc;
+        this.GetCameraZoomFunc = GetCameraZoomFunc;
+    }
+
+    private void Start()
+    {
+        myCamera = transform.GetComponent<Camera>();
+    }
+
+    public void SetCameraFollowPosition(Vector3 cameraFollowPosition)
+    {
+        SetGetCameraFollowPositionFunc(() => cameraFollowPosition);
+    }
+    public void SetGetCameraFollowPositionFunc(Func<Vector3> GetFollowPositionFunc)
     {
         this.GetFollowPositionFunc = GetFollowPositionFunc;
     }
+
+    public void SetCameraZoom(float cameraZoom)
+    {
+        SetGetCameraZoomFunc(() => cameraZoom);
+    }
+    public void SetGetCameraZoomFunc(Func<float> GetCameraZoomFunc)
+    {
+        this.GetCameraZoomFunc = GetCameraZoomFunc;
+    }
     void Update()
+    {
+        HandleMovement();
+        HandleZoom();
+
+    }
+
+    private void HandleMovement()
     {
         Vector3 cameraFollowPosition = GetFollowPositionFunc();
         cameraFollowPosition.z = transform.position.z;
-        transform.position = cameraFollowPosition;
+
+        Vector3 cameraMoveDir = (cameraFollowPosition - transform.position).normalized;
+        float distance = Vector3.Distance(cameraFollowPosition, transform.position);
+        float cameraMoveSpeed = 2f;
+
+        if (distance > 0)
+        {
+            Vector3 newCameraPosition = transform.position + cameraMoveDir * distance * cameraMoveSpeed * Time.deltaTime;
+
+            float distanceAfterMoving = Vector3.Distance(newCameraPosition, cameraFollowPosition);
+
+            if (distanceAfterMoving > distance)
+            {
+                //OvershottheTarget;
+
+                newCameraPosition = cameraFollowPosition;
+            }
+
+            transform.position = newCameraPosition;
+        }
+    }
+
+    void HandleZoom()
+    {
+        float cameraZoom = GetCameraZoomFunc();
+
+        float cameraZoomDifference = cameraZoom - myCamera.orthographicSize;
+        float cameraZoomSpeed = 1f; 
+        myCamera.orthographicSize += cameraZoomDifference * cameraZoomSpeed * Time.deltaTime;
+        if(cameraZoomDifference > 0)
+        {
+            if(myCamera.orthographicSize > cameraZoom)
+            {
+                myCamera.orthographicSize = cameraZoom;
+            }
+        }
+        else
+        {
+            if (myCamera.orthographicSize < cameraZoom)
+            {
+                myCamera.orthographicSize = cameraZoom;
+            }
+        }
     }
 }
--- FirstCodeMonkeyLesson/Assets/Scripts/GameHandler.cs
+++ FirstCodeMonkeyLesson/Assets/Scripts/GameHandler.cs
@@ -7,12 +7,20 @@ using CodeMonkey.Utils;
 
 public class GameHandler : MonoBehaviour {
 
+    public Material material;
+
     public Transform pfHealthBar;
     public CameraFollow cameraFollow;
+    public Transform playerTransform;
 
-    void Start() {
+    private float zoom = 40f;
 
-        cameraFollow.Setup(() => new Vector3(0, -100));
+
+    void Start() 
+    {
+
+        cameraFollow.Setup(() => playerTransform.position, () => zoom);
+        cameraFollow.SetGetCameraFollowPositionFunc(() => playerTransform.position);
 
         HealthSystem healthSystem = new HealthSystem(100);
 
@@ -33,6 +41,72 @@ public class GameHandler : MonoBehaviour {
             healthSystem.Heal(10);
             Debug.Log("Healed:" + healthSystem.GetHealth());
         });
+
+        CMDebug.ButtonUI(new Vector2(570, 90), "ZoomIn", ZoomIn);
+
+
+        CMDebug.ButtonUI(new Vector2(570, 30), "ZoomOut", ZoomOut);
+
+        Vector3[] vertices = new Vector3[4];
+        Vector2[] uv = new Vector2[4];
+        int[] triangles = new int[6];
+
+        vertices[0] = new Vector3(0,1);
+        vertices[1] = new Vector3(1,1);
+        vertices[2] = new Vector3(0,0);
+        vertices[3] = new Vector3(1,0);
+
+        uv[0] = new Vector2(0,1);
+        uv[1] = new Vector2(1,1);
+        uv[2] = new Vector2(0,0);
+        uv[3] = new Vector2(1,0);
+
+        int headX = 0;
+        int headY = 380;
+        int headWidth = 128;
+        int headHeight = 128;
+        int textureWidth = 512;
+        int textureHeaight = 512;
+
+        uv[0] = ConvertPixelsToUVCoordinates(headX, headY + headHeight, textureWidth, textureHeaight);
+        uv[1] = ConvertPixelsToUVCoordinates(headX + headWidth, headY + headHeight, textureWidth, textureHeaight);
+        uv[2] = ConvertPixelsToUVCoordinates(headX, headY, textureWidth, textureHeaight);
+        uv[3] = ConvertPixelsToUVCoordinates(headX + headWidth, headY, textureWidth, textureHeaight);
+
+        triangles[0] = 0;
+        triangles[1] = 1;
+        triangles[2] = 2;
+        triangles[3] = 2;
+        triangles[4] = 1;
+        triangles[5] = 3;
+
+        Mesh mesh = new Mesh();
+
+        mesh.vertices = vertices;
+        mesh.uv = uv;
+        mesh.triangles = triangles;
+
+        GameObject gameObject = new GameObject("Mesh", typeof(MeshFilter), typeof(MeshRenderer));
+        gameObject.transform.localScale = new Vector3(30, 30, 1);
+
+        gameObject.GetComponent<MeshFilter>().mesh = mesh;
+        gameObject.GetComponent<MeshRenderer>().material = material;
+    }
+
+    public Vector2 ConvertPixelsToUVCoordinates(int x, int y , int textureWidth,int textureHeight)
+    {
+        return new Vector2((float)x/textureWidth,(float)y/textureHeight);
+    }
+    void ZoomIn()
+    {
+        zoom -= 40f;
+
+        if (zoom < 40f) zoom = 40f;
+    }
+    void ZoomOut()
+    {
+        zoom += 40f;
+        if (zoom > 200f) zoom = 200f;
     }
 }
 
